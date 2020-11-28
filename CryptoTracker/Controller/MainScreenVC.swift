5819c844406657ffd943b332ce4f47aee7450fa6
Taimuras <Avelonadreamer@gmail.com>
2020-11-28 15:19:10 +0600

    Refreshing swipe

--- CryptoTracker/Controller/MainScreenVC.swift
+++ CryptoTracker/Controller/MainScreenVC.swift
@@ -9,6 +9,7 @@ class MainScreenVC: UIViewController{
     @IBOutlet weak var myCollectionView: UICollectionView!
     
     let networking = Networking()
+    let refreshControl = UIRefreshControl()
     var endPoint = [CoinsModel]()
     let urlHead = "https://www.cryptocompare.com"
     
@@ -35,6 +36,37 @@ class MainScreenVC: UIViewController{
             }
             self.myCollectionView.reloadData()
         }
+        
+        
+        
+        refreshControl.tintColor = .white
+        refreshControl.addTarget(self, action: #selector(MainScreenVC.refreshData), for: .valueChanged)
+        refreshControl.attributedTitle = NSAttributedString(string: "Refreshing", attributes: nil)
+        myCollectionView.addSubview(refreshControl)
+        
+        
+        
+        
+    }
+    @objc func refreshData() {
+        endPoint.removeAll()
+        print(endPoint.count)
+        networking.fetchingData { (data) in
+            let json = JSON(data)
+            
+            for i in 0 ..< json.count {
+                let coinFullName = json[i]["CoinInfo"]["FullName"].stringValue
+                let priceUSD = json[i]["DISPLAY"]["USD"]["PRICE"].stringValue
+                let imageURL = self.urlHead + json[i]["CoinInfo"]["ImageUrl"].stringValue
+                let coin = CoinsModel(fullName: coinFullName, priceUSD: priceUSD, iamgeURL: imageURL)
+                self.endPoint.append(coin)
+            }
+            self.refreshControl.endRefreshing()
+            self.myCollectionView.reloadData()
+            print(self.endPoint.count)
+            
+        }
+        
     }
 }
 
@@ -62,6 +94,7 @@ extension MainScreenVC: UICollectionViewDelegate, UICollectionViewDataSource, UI
         let width  = view.frame.width - 20
         return CGSize(width: width, height: 70.0)
     }
+    
 }
 
 
